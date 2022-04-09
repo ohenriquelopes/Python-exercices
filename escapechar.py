9e1453a8c3f39b1f3d66bdff9f52d548b9e90058
Henrique L <hlopessantos52@gmail.com>
2022-04-09 16:29:44 -0300

    escapechar.py

---
+++ escapechar.py
@@ -0,0 +1,21 @@
+splitString = "This string has benn \nsplit over \nseveral\nlives"
+print(splitString)
+
+tabbetString = "1\t2\t3\t4\t5"
+print(tabbetString)
+
+print("The pet shop owner said 'No, no, \'e\'s uh, ...he\'s resting.'")
+
+anotherSplitString = """
+This string has been \
+    split overz \
+    several \
+    lines
+"""
+
+print(anotherSplitString)
+
+print("c:\\Users\\timbuchalka\\notes.txt")
+print(r"C:\Users\timbuchalka\notes.txt")
+
+print("  ")
