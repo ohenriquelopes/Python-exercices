d72e408ba75b3f072c3426d894abbc41190b0435
Henrique L <hlopessantos52@gmail.com>
2022-04-10 18:33:40 -0300

    add itens in strings2

--- world_of_python/strings2.py
+++ world_of_python/strings2.py
@@ -13,4 +13,34 @@ values = "".join(char if char not in seperators else " " for char in number).spl
 print([int(val) for val in values])
 
 
+print(parrot)
+print(parrot[3])
+print()
 
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+print()
+print()
+
+print(parrot[0:6])
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
+
+print()
+
+print(parrot[10:14])
+print(parrot[10:])
+
+
+print(parrot[:6])
+print(parrot[6:])
+
+
+print(parrot[:6]) + print(parrot[6:])
+print(parrot[:])
