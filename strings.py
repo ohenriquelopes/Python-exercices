0e5d7454480f8f0608fe9c8a341c5d0237a0a425
Henrique L <hlopessantos52@gmail.com>
2022-04-10 12:20:41 -0300

    add content strings.py

---
+++ strings.py
@@ -0,0 +1,18 @@
+greeting = "aaaaa"
+name = "Henrique"
+
+print(greeting + ' ' + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(name))
+
+age_in_words = "2 years"
+print(name + f" is {age} years old")
+print(type(age))
+
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
